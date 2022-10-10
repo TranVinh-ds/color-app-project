f7c49aa0bb36d936c175fb7e6d421ee3a84b5761
TranVinh-ds <chenvinh2003@gmail.com>
2022-10-10 16:17:43 +0700

    create square and color input components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import Square from './components/Square';
+import ColorInput from './components/ColorInput';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [colorValue, setColorValue] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Square colorValue={colorValue} />
+      <ColorInput colorValue={colorValue} setColorValue={setColorValue} />
     </div>
   );
 }
